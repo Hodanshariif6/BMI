e314509280d4563a2abb7060cd48c1ad7c879d50
hodanshariif6 <hodanshariif617@gmail.com>
2025-07-14 23:35:17 -0700

    bmi2

--- bmi.js
+++ bmi.js
@@ -1,25 +1,26 @@
-   function calculateBMI() {
-      const height = parseFloat(document.getElementById("height").value);
-      const weight = parseFloat(document.getElementById("weight").value);
-      const resultDiv = document.getElementById("result");
+function calculateBMI() {
+const height = parseFloat(document.getElementById("height").value);
+const weight = parseFloat(document.getElementById("weight").value);
+const resultDiv = document.getElementById("result");
 
-      if (!height || !weight || height <= 0 || weight <= 0) {
-        resultDiv.innerHTML = "invailed";
-        return;
-      }
+if (!height || !weight || height <= 0 || weight <= 0) {
+  resultDiv.innerHTML = "invailed";
+  return;
+}
 
-      const heightInMeters = height / 100;
-      const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
-      let message = "";
+const heightInMeters = height / 100;
+const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
+let message = "";
 
-      if (bmi < 18.5) {
-        message = `Your BMI is <strong>${bmi}</strong>  Underweight `;
-      } else if (bmi < 25) {
-        message = `Your BMI is <strong>${bmi}</strong>  Normal `;
-      } else if (bmi < 30) {
-        message = `Your BMI is <strong>${bmi}</strong>  Overweight `;
-      } else {
-      }
+if (bmi < 18.5) {
+  message = `Your BMI is <strong>${bmi}</strong>  Underweight `;
+  
+} else if (bmi < 25) {
+  message = `Your BMI is <strong>${bmi}</strong>  Normal `;
+} else if (bmi < 30) {
+  message = `Your BMI is <strong>${bmi}</strong>  Overweight `;
+} else {
+}
 
-      resultDiv.innerHTML = message;
-    }
+resultDiv.innerHTML = message;
+ }
